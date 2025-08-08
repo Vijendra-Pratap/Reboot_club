22f65f864265b8df33339357d463a5c502a166bd
Vijendra Pratap Singh <vijendraprataps1390@gmail.com>
2025-08-08 14:53:15 +0000

    make different logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -13,9 +13,9 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-      <path d="M12 9v6" />
-      <path d="M9 12h6" />
+      <path d="M12 5v14" />
+      <path d="M5 12h14" />
+      <path d="M19 12a7 7 0 1 0-14 0 7 7 0 0 0 14 0z" />
     </svg>
   );
 }
